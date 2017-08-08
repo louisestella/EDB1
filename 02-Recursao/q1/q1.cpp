f504e080b8597ba40bb708d35daafb993c974019
Louise <louiselbarbosa@gmail.com>
2017-08-08 20:24:59 -0300

    recursion exercises q1 initialized

--- 02-Recursao/q1/q1.cpp
+++ 02-Recursao/q1/q1.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
+void reverseString( string s );
+
 int main()
 {
-    string s = "i just don't know what to do with myself";
+    string s = "flesym htiw od ot tahw wonk t'nod tsuj I";
 
+    cout << s << endl;
+
+    cout << reverseString( s ) << endl; 
+
+    return 0;
     
+}
+
+//recursive function to revert a string 
+void reverseString( string s )
+{
+
+
+
 }
