feb4d4071207c816b26f71b9d45dc7120f789654
Louise <louiselbarbosa@gmail.com>
2017-08-12 16:13:01 -0300

    revert string recursive function

---
+++ 02-Recursao/q1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+string reverseString( string str, string reversed, int size  );
+
+int main()
+{
+    string str = "flesym htiw od ot tahw wonk t'nod tsuj I";
+    string reversed = "";
+
+    cout << "Type a text to see it reversed..." << endl << endl; 
+    getline(cin, str);
+
+    int size = str.length();
+
+    cout << reverseString( str, reversed, size ) << endl; 
+
+    return 0;
+    
+}
+
+//recursive function to revert a string 
+string reverseString( string str, string reversed, int size )
+{
+
+    if ( size <= 0 )
+    {
+        reversed = reversed + str[size];
+        return reversed;
+    }
+    else
+    {
+        reversed = reversed + str[size];
+        return reverseString( str, reversed, size-1 );
+    }
+
+}
