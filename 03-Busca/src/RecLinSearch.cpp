7104c68feb3f605c915ae91df1e3c2e1c5cfb249
Louise <louiselbarbosa@gmail.com>
2017-08-13 23:29:18 -0300

    sequential and binary search exercises

---
+++ 03-Busca/src/RecLinSearch.cpp
@@ -0,0 +1,13 @@
+//
+//  RecLinearSearch.cpp
+//  Recursive Linear Search
+//
+//  Created by Eiji Adachi Medeiros Barbosa 
+//
+
+#include "Search.hpp"
+
+int search(int v[], int size, int key)
+{
+    // TO DO
+}
