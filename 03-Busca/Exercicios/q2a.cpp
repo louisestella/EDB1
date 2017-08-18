2c84ce0ecedb335a8615594e95e38bc5f169e5ad
Louise <louiselbarbosa@gmail.com>
2017-08-17 21:26:06 -0300

    function that returns the index number of a given key

---
+++ 03-Busca/Exercicios/q2a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int findIndex(int v[], int size, int key);
+
+int main(){
+    int list[] = { 0, 1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10, 12, 35, 37, 37, 38, 40 };
+    int size = 21;
+    int key = -1;
+
+    cout << endl << "This is your array:" << endl << "[";    
+    for( int i = 0; i < size; i++ ){
+        cout << list[i];
+        if ( i < size-1 ){
+            cout << ", ";
+        }
+    }
+    cout << "]." << endl;
+
+    cout << "Choose a KEY to search for in this array." << endl;
+    cin >> key;
+
+    cout << findIndex( list, size, key ) << endl;
+
+    
+    
+    return 0;
+}
+
+//function to return the index number of a given key (if there is one)
+int findIndex(int v[], int size, int key){
+    int begin = 0;
+    int end = size-1;
+    int middle = (begin + end) / 2;
+
+    while( begin <= end ){
+        middle = (begin + end) / 2;
+        if( v[middle] == v[middle-1] || v[middle] > key ){
+            end = middle - 1;
+        } else if( v[middle] < key ){
+            begin = middle + 1;
+        } else{
+            return middle;
+        }
+    }
+    return -1; // NOT_FOUND
+}
